fb8907b5272664625cbea05955cf7490fced7843
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-09-17 20:49:26 -0500

    add: Add navbar responsive mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './style/index.css'
 import '@fontsource/poppins';
 
 createRoot(document.getElementById('root')!).render(
